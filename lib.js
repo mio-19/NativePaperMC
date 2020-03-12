dbde71cbd37323237dc8428173d569673dcb6eee
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2020-03-12 15:34:56 +0800

    更

--- lib.js
+++ lib.js
@@ -601,7 +601,7 @@ async function get_files_list(path) {
   const result = []
   for (const f of await find(".")) {
     if ((await stat(f)).isFile()) {
-      result.push("./" + f)
+      result.push(f)
     }
   }
   await popd()
